6190503f2cbe1c0b67059cd878d70f2665269be9
VanGriffith <vc13grif@siena.edu>
2020-04-23 17:55:33 -0400

    updated paint and press methods

--- MinesweeperGame/Tile.java
+++ MinesweeperGame/Tile.java
@@ -1,46 +1,64 @@
 import javax.swing.JButton;
+import javax.swing.JComponent;
+import java.awt.Point;
+import java.awt.Graphics;
+import java.awt.Color;
 /**
  * Write a description of class Tile here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Tile extends JButton
+public class Tile extends Thread
 {
+    private final int SIZE = 50;
+    
     private int number;
     private int row;
     private int col;
     private boolean isBomb;
+    private Point upperLeft;
+    private JComponent container;
+    private boolean isPressed;
     
-    public Tile(int number, int row, int col, boolean isBomb)
+
+    public Tile(int number, int row, int col, Point upperLeft, JComponent container)
     {
-        super();
         this.number = number;
         this.row = row;
         this.col = col;
-        this.isBomb = isBomb;
+        if (number == -1)
+        {
+            isBomb = true;
+        }
+        else
+        {
+            isBomb = false;
+        }
+        this.upperLeft = upperLeft;
+        this.container = container;
     }
-    
+
     public int getNumber()
     {
-       return number; 
+        return number; 
     }
-    
+
     public int getRow()
     {
-       return row; 
+        return row; 
     }
-    
+
     public int getCol()
     {
-       return col; 
+        return col; 
     }
-    
+
     public boolean isBomb()
     {
-       return isBomb; 
+        return isBomb; 
     }
-    
+
     public void setNumber(int number)
     {
         this.number = number;
@@ -49,10 +67,37 @@ public class Tile extends JButton
             isBomb = true;
         }
     }
-    
+
     public void incrementNumber()
     {
         number++;
     }
+
+    public int getTileSize()
+    {
+        return SIZE;
+    }
     
+    public void paint(Graphics g)
+    {
+        if (isPressed)
+        {
+            g.setColor(Color.GRAY);
+            g.fillRect(upperLeft.x, upperLeft.y, SIZE, SIZE);
+        }
+        g.setColor(Color.BLACK);
+        g.drawRect(upperLeft.x, upperLeft.y, SIZE, SIZE);
+    }
+    
+    public void press()
+    {
+        if (isPressed)
+        {
+            isPressed = false;
+        }
+        else
+        {
+            isPressed = true;
+        }
+    }
 }
