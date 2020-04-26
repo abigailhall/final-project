4d8f9b9158cfd78c22dcdc948ceb744c7950fb10
abigailhall <ar22hall@siena.edu>
2020-04-26 16:51:41 -0400

    all comments are up to date

--- FruitNinjaGame/RedLine.java
+++ FruitNinjaGame/RedLine.java
@@ -2,12 +2,12 @@ import java.awt.Point;
 import javax.swing.JComponent;
 import java.awt.Graphics;
 import java.awt.Color;
-import java.util.Random;
-
 /**
- * Write a description of class RedLine here.
+ * This RedLine class extends the abstract class 'AnimatedLine' in order to have all of its proper
+ * functionality so it appears as a sword. This class makes it so that the sword red and it 
+ * looks like it also vanishes while moving it.
  *
- * @author Kate Frisch
+ * @author Kate Frisch, based heavily off of the animations lab
  * @version Spring 2020
  */
 public class RedLine extends AnimatedLine
@@ -15,18 +15,36 @@ public class RedLine extends AnimatedLine
 
     public int colorNum;
 
+    /**
+     * Construct a new RedLine object at the given position.
+     * 
+     * @param start the starting point
+     *        end the ends point
+     *        panel the Component in which this Fruit will live
+     *        diffLevel the difficulty level the user selects at the beginning of the game.
+     */
     public RedLine(Point start, Point end, JComponent container)
     {
         super(start, end, container);
         colorNum = 255;
     }
 
+    /**
+     * Will draw the RedLine at given position. ColorNum is used to keep track of 
+     * the decrease in color (red).
+     * 
+     * @param g the Graphics object in which to paint
+     */
     public void paint(Graphics g) 
     {
         g.setColor(new Color(colorNum, 0, 0));
         g.drawLine(start.x, start.y, end.x, end.y);
     }
 
+    /**
+     * Run method to define the life of the RedLine. This method is what makes
+     * it have its 'sword' appearance. 
+     */
     @Override
     public void run() {
 
