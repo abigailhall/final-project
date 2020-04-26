c995583cc6fd17113edc4be76ff2732bde3e2ec1
abigailhall <ar22hall@siena.edu>
2020-04-26 16:30:35 -0400

    all comments are up to date

--- FruitNinjaGame/AnimatedLine.java
+++ FruitNinjaGame/AnimatedLine.java
@@ -2,9 +2,10 @@ import java.awt.Point;
 import javax.swing.JComponent;
 import java.awt.Graphics;
 /**
- * Write a description of class AnimatedLine here.
+ * This abstract class is responsible for creating and animating the sword, which will then
+ * be painted with seperate classes.
  *
- * @author Kate Frisch
+ * @author Kate Frisch, taken from the animations lab
  * @version Spring 2020
  */
 public abstract class AnimatedLine extends Thread
@@ -15,7 +16,15 @@ public abstract class AnimatedLine extends Thread
     protected Point end;
     protected JComponent container;
     protected boolean done;
-    
+
+    /**
+     * Construct a new AnimatedLine object at the given position.
+     * 
+     * @param start the starting point
+     *        end the ends point
+     *        panel the Component in which this Fruit will live
+     *        diffLevel the difficulty level the user selects at the beginning of the game.
+     */
     public AnimatedLine(Point start, Point end, JComponent container)
     {
         this.start = start;
@@ -23,8 +32,19 @@ public abstract class AnimatedLine extends Thread
         this.container = container;
     }
 
+    /**
+     * Abstract paint method that will change depending on which type of sword is chosen by 
+     * the user.
+     * 
+     * @param g the Graphics object in which to paint
+     */
     public abstract void paint(Graphics g);
 
+    /**
+     * Check if this AnimatedLine's work is done.
+     * 
+     * @return true if this AnimatedLine's work is done
+     */
     public boolean done() 
     {
         return done;
