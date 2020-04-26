623c399c7f79d92e137377db550fa77f383e6ef6
abigailhall <ar22hall@siena.edu>
2020-04-26 16:46:25 -0400

    all comments are up to date

--- FruitNinjaGame/RainbowLine.java
+++ FruitNinjaGame/RainbowLine.java
@@ -2,12 +2,12 @@ import java.awt.Point;
 import javax.swing.JComponent;
 import java.awt.Graphics;
 import java.awt.Color;
-import java.util.Random;
-
 /**
- * Write a description of class RainbowLine here.
+ * This RainbowLine class extends the abstract class 'AnimatedLine' in order to have all of its proper
+ * functionality so it appears as a sword. This class makes it so that the sword is rainbow, while also
+ * having a vanishing appearance to it.
  *
- * @author Kate Frisch
+ * @author Kate Frisch, taken from the animations lab
  * @version Spring 2020
  */
 public class RainbowLine extends AnimatedLine
@@ -18,6 +18,14 @@ public class RainbowLine extends AnimatedLine
     private int xDist;
     private int yDist;
 
+    /**
+     * Construct a new RainbowLine object at the given position.
+     * 
+     * @param start the starting point
+     *        end the ends point
+     *        panel the Component in which this Fruit will live
+     *        diffLevel the difficulty level the user selects at the beginning of the game.
+     */
     public RainbowLine(Point start, Point end, JComponent container)
     {
         super(start, end, container);
@@ -30,6 +38,12 @@ public class RainbowLine extends AnimatedLine
         yDist = yDist < 5 ? 5 : yDist;
     }
 
+    /**
+     * Will draw the RainbowLine at given position. Color will be set/determined
+     * by the run method. It will have many colors, like a rainbow.
+     * 
+     * @param g the Graphics object in which to paint
+     */
     public void paint(Graphics g) 
     {
 
@@ -38,6 +52,10 @@ public class RainbowLine extends AnimatedLine
 
     }
 
+    /**
+     * Run method to define the life of the RainbowLine. This method is what makes
+     * it have different colors and have its 'sword' appearance. 
+     */
     @Override
     public void run() {
 
@@ -48,45 +66,46 @@ public class RainbowLine extends AnimatedLine
             }
             catch (InterruptedException e) {
             }
+
             switch(nextNum)
             {
                 case 0:
                 color = new Color(255, 69, 0);
                 break;
-                
+
                 case 1:
                 color = Color.ORANGE;
                 break;
-                
+
                 case 2:
                 color = Color.YELLOW;
                 break;
-                
+
                 case 3:
                 color = new Color(173, 255, 47);
                 break;
-                
+
                 case 4:
                 color = Color.GREEN;
                 break;
-                
+
                 case 5:
                 color = Color.CYAN;
                 break;
-                
+
                 case 6:
                 color = Color.BLUE;
                 break;
-                
+
                 case 7:
                 color = new Color(138, 43, 226);
                 break;
-                
+
                 default:
                 color = Color.MAGENTA;
                 break;
             }
-            
+
             nextNum++;
             container.repaint();
         }
