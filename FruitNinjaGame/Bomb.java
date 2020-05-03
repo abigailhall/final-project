21ee33e2e0133562f5b7d6c7092be10989b6f2e4
VanGriffith <vc13grif@siena.edu>
2020-05-03 16:44:33 -0400

    bomb now plays bomb sound when hit

--- FruitNinjaGame/Bomb.java
+++ FruitNinjaGame/Bomb.java
@@ -2,6 +2,7 @@ import java.awt.Image;
 import java.awt.Graphics;
 import java.awt.Toolkit;
 import javax.swing.JComponent;
+import java.awt.Point;
 /**
  * This Bomb class extends the abstract class 'Fruit' in order to have all of its proper
  * functionality. This class loads the proper bomb picture, so that when it is thrown,
@@ -35,6 +36,26 @@ public class Bomb extends Fruit
     public void paint(Graphics g)
     {
         g.drawImage(fruitPic, (int)upperLeftX, (int)upperLeftY, fruitPicHeight, fruitPicHeight, null);
+    }
+    
+    /**
+     * Check if the mouse overlaps the fruit.
+     */
+    @Override
+    public int mouseOverlapsFruit(Point mousePos)
+    {
+        if (mousePos == null) return 0;
+        int radius = fruitPicHeight / 2;
+
+        if (!isSliced && mousePos.distance(new Point((int) upperLeftX + radius, (int) upperLeftY  + radius)) < radius)
+        {
+            isSliced = true;
+            ySpeed = 10;
+            Audio.playSound("bombSound.wav");
+            return pointValue;
+        }
+
+        return 0;
     }
 
     /**
